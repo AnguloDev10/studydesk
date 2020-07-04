a6308b92c41c9cd2a36c0e3365ff2a2706be138b
Omar Alonso Clavijo Mestas <u201817916@upc.edu.pe>
2020-07-04 10:36:03 -0500

    Create-Endpoints-Courses

--- src/main/java/com/studydesk/Service/CourseService.java
+++ src/main/java/com/studydesk/Service/CourseService.java
@@ -1,5 +1,6 @@
 package com.studydesk.Service;
 
+import com.studydesk.Model.Career;
 import com.studydesk.Model.Course;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,9 +8,11 @@ import org.springframework.http.ResponseEntity;
 
 public interface CourseService {
     Page<Course> getAllCourses(Pageable pageable);
-    Page<Course> getAllTagsByCareerId(Long careerId, Pageable pageable);
+    Page<Course> getAllCoursesByCareerId(Long careerId, Pageable pageable);
     Course getCourseById(Long courseId);
-    Course createCourse(Course course);
+    Course createCourse(Course courseId);
     Course updateCourse(Long courseId, Course courseDetails);
     ResponseEntity<?> deleteCourses(Long courseId);
+    Course assignCourseCareer(Long courseId, Long careerId);
+    Course unassignCourseCareer(Long courseId, Long careerId);
 }
